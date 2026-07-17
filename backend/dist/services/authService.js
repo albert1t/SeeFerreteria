c53f3b48950c4ff2c3c4d14cc5d968aa922f16ff
ThEduuuu <thedu241@gmail.com>
2026-07-17 10:09:07 +0200

    fix: compilar backend con bcryptjs

--- backend/dist/services/authService.js
+++ backend/dist/services/authService.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { randomBytes } from 'crypto';
 import { createRemoteJWKSet, jwtVerify } from 'jose';
 import * as usersRepo from '../repositories/users.js';
